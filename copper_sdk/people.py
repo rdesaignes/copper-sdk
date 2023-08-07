b056781e3ab1c22e1e77fd418fbdc7ce61bc9948
rdesaignes <rdesaignes@ccld.com>
2023-08-07 15:02:39 +0200

    Update people.py
    Correct default value in People list function

--- copper_sdk/people.py
+++ copper_sdk/people.py
@@ -51,7 +51,7 @@ class People(BaseResource):
         default_body = {
             'page_number': 1, # number	The page number (starting with 1) that you would like to view.	1
             'page_size': 20, # number	The number of entries included in a page of results	20
-            'sort_by': 'first_name',  # string	The field on which to sort the results (see footnote 1).
+            'sort_by': 'name',  # string	The field on which to sort the results (see footnote 1).
             'sort_direction': 'asc',  # string	The direction in which to sort the results. Possible values are: asc or desc.
         }
 
